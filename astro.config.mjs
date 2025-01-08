1499a40fca17dd8652118fa83dbc42353c340b8b
varunsinghal <varunsinghal3655@>
2025-01-08 13:08:40 +0530

    grievance form ,events page added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sdcopmzn.com",
+      },
+    ],
+  },
 });
