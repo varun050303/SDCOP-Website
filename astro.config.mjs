9193f84ed6d78b0ab9f1e60bc39a6524e13580a6
varunsinghal <varunsinghal3655@>
2025-01-12 17:18:06 +0530

    minor fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   image: {
     remotePatterns: [
       {
